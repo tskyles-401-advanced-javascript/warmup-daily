b5c87b799de54a9c776b176aaa33e7d388eaf7c3
travis skyles <travis.skyles@gmail.com>
2020-01-13 10:49:50 -0800

    created for and while loops

---
+++ 01-13-2020/warmup.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const arr = [1,2,3,4,5,6,7,8,9,10];
+
+const forLoop(arr) => {
+  for(let i=0; i<arr.length; i++){
+    console.log(arr[i]);
+  };
+}
+
+const whileLoop(arr) => {
+  let i = 0;
+  while(i < arr.length){
+    console.log(arr[i]);
+  };
+}

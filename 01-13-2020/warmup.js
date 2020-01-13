39ddc67547e69e453d029cea592344312224881f
travis skyles <travis.skyles@gmail.com>
2020-01-13 10:52:49 -0800

    map

--- 01-13-2020/warmup.js
+++ 01-13-2020/warmup.js
@@ -2,15 +2,23 @@
 
 const arr = [1,2,3,4,5,6,7,8,9,10];
 
-const forLoop(arr) => {
+const forLoop = (arr) => {
   for(let i=0; i<arr.length; i++){
     console.log(arr[i]);
   };
 }
 
-const whileLoop(arr) => {
+const whileLoop = (arr) => {
   let i = 0;
   while(i < arr.length){
     console.log(arr[i]);
   };
+}
+
+const map = (arr, cb) => {
+  const newarr = [];
+  for(let i=0; i<arr.length; i++){
+    newarr.push(cb(arr[i]));
+  };
+  return newarr;
 }

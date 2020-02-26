e434d22f2beab1e4a90a955b60e5e07c1761e517
travis skyles <travis.skyles@gmail.com>
2020-02-26 11:04:58 -0800

    arrays

---
+++ 02-26-2020/warmup.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const forLoop = (arr) => {
+  for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+  };
+}
+
+const whileLoop = (arr) => {
+  let count = 0;
+  while(count < arr.length){
+    console.log(arr[i]);
+  };
+}
+
+const map = (arr, cb) => {
+  const newArr = [];
+  for(let i = 0; i < arr.length; i++){
+    newArr.push(cb(arr[i]));
+  };
+  return newArr;
+}
+
+const filter = (arr, cb) => {
+  const newArr = [];
+  for(let i = 0; i < arr.length; i++){
+    if(cb(arr[i])){
+      newArr.push(arr[i]);
+    };
+  };
+  return newArr;
+}
+
+const reduce = (arr, cb, newThing) => {
+  for(let i = 0; i < arr.length; i++){
+    newThing = cb(newThing, arr[i], i);
+  }
+  return newThing;
+}
